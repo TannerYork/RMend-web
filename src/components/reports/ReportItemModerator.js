7040f082420adb002bed6107b6661a03d75800d6
Tanner York <tannerwyork@gmail.com>
2019-09-22 14:55:41 -0700

    [Update] Finnish style changes to report item cards

--- src/components/reports/ReportItemModerator.js
+++ src/components/reports/ReportItemModerator.js
@@ -11,41 +11,16 @@ class ReportItemModerator extends React.Component {
         <figure className="report-item__figure">
           <img
             className="report-item__img"
-            src={data.imageUrl || '../../images/Spin-1s-80px.svg'}
+            src={data.photos[0].imageUrl || '../../images/Spin-1s-80px.svg'}
             alt={data.details}
           />
         </figure>
-        <div class="report-item_actions">
-        <button
-          className="report-item__button report-item__button-delete .js-report-item-delete"
-          title="Delete"
-        />
-        <button
-          className="report-item__button report-item__button-print .js-report-item-print"
-          title="Print"
-        />
-        <label
-          className="report-item__button-prioritize .js-report-item-prioritize"
-          title="Prioritize"
-        >
-          <input type="checkbox" 
-          className="checkbox js-priority" 
-          defaultChecked={data.priority} 
-          />
-          <span className="checkmark" />
-        </label>
-        <label
-          className="report-item__button-underReview .js-report-item-underReview"
-          title="put report under review"
-        >
-          <input
-            type="checkbox"
-            className="checkbox js-review"
-            checked={data.isBeingReviewed}
-            disabled={data.isBeingReviewed}
-          />
-          <span className="checkmark" />
-        </label>
+        <div className="report-item__preview">
+        <div className="report-item__details">
+          <h3>{data.sender}</h3>
+          <h4>{data.roadName}</h4>
+        </div>
+        <button className="report-item__button">More</button>
         </div>
       </div>
     );
--- src/components/reports/ReportItemStaff.js
+++ src/components/reports/ReportItemStaff.js
@@ -4,47 +4,21 @@ import './ReportItem.css';
 const StaffReportItem = props => {
   const { data } = props;
   return (
-    <div class="report-item js-report-item" id={data.id} data-id={data.id}>
-      <figure class="report-item__figure">
+    <div className="report-item js-report-item" id={data.id}>
+      <figure className="report-item__figure">
         <img
-          class="report-item__img"
-          src={data.imageUrl || '../../images/Spin-1s-80px.svg'}
+          className="report-item__img"
+          src={data.photos[0].imageUrl || '../../images/Spin-1s-80px.svg'}
           alt={data.details}
         />
       </figure>
-      <div class="report-item__data disable-scrollbars">
-        <h4>
-          <span class="report-item__subheader">Sender: </span>
-          {data.sender}
-        </h4>
-        <p>
-          <span class="report-item__subheader">Date: </span>
-          {data.timestamp}
-        </p>
-        <p>
-          <span class="report-item__subheader">Road Name: </span>
-          {data.roadName}
-        </p>
-        <p>
-          <span class="report-item__subheader">Description: </span>
-          {data.details}
-        </p>
-        <p>
-          <span class="report-item__subheader">Nearest Street: </span>
-          {data.nearestStreet}
-        </p>
-        <p>
-          <span class="report-item__subheader">Magisterial District: </span>
-          {data.magisterialDistrict}
-        </p>
+      <div className="report-item__preview">
+      <div className="report-item__details">
+        <h3>{data.sender}</h3>
+        <h4>{data.roadName}</h4>
       </div>
-      <label
-        class="report-item__button-underReview .js-report-item-underReview"
-        title="put report under review"
-      >
-        <input type="checkbox" class="checkbox js-review" checked={data.isBeingReviewed} disabled />
-        <span class="checkmark" />
-      </label>
+      <button className="report-item__button">More</button>
+      </div>
     </div>
   );
 };
