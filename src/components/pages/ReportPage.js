23597fab2ee18c75a788da40ae3055668d21776a
Tanner York <tannerwyork@gmail.com>
2019-09-24 18:29:07 -0700

    [Update] Restyle report page

--- src/components/pages/ReportPage.js
+++ src/components/pages/ReportPage.js
@@ -1,35 +1,72 @@
 import React from 'react';
+
+import DetailsCard from '../reports/DetailsCard';
 import './ReportPage.css';
 
-class ReportPage extends React.Component{
-    renderPhotos = () => {
-        const { data } = this.props
-        return data.photos.map((photo) => {
-            return <img src={photo.imageUrl} alt={data.details} />
-        });
-    }
+class ReportPage extends React.Component {
+  renderPhotos = () => {
+    const { data } = this.props;
+    return data.photos.map(photo => {
+      return <img src={photo.imageUrl} alt={data.details} />;
+    });
+  };
 
-    render() {
-        const { data } = this.props
-        return (
-           <div>
-                <div className="report-photos">
-                    {this.renderPhotos()}
-                </div>
-                <div className="form-header report-header">
-                    <h1>{data.sender}</h1>
-                </div>
-                <div className="report-details">
-                    <p>Details: {data.details}</p>
-                    <p>Under Review: {data.isBeingReviewd}</p>
-                    <p>District: {data.magisterialDiscrict}</p>
-                    <p>Nearest Street: {data.nearestStreet}</p>
-                    <p>Road Name: {data.roadName}</p>
-                    <p>Date: {data.timestamp}</p>
-                </div>
-           </div>
-        );
-    }
+  handleCheckboxChange = changeEvent => {
+    const { name } = changeEvent.target;
+
+    this.setState(prevState => ({
+      checkboxes: {
+        ...prevState.checkboxes,
+        [name]: !prevState.checkboxes[name]
+      }
+    }));
+  };
+
+  render() {
+    const { data } = this.props;
+    return (
+      <div className="report-page">
+        <div className="report-photos">{this.renderPhotos()}</div>
+        <div className="report-details">
+          <DetailsCard label="Sender">
+            <h4>{data.sender}</h4>
+          </DetailsCard>
+          <DetailsCard label="Detials">
+            <h4>{data.details}</h4>
+          </DetailsCard>
+          <DetailsCard label="District">
+            <h4>{data.magisterialDistrict}</h4>
+          </DetailsCard>
+          <DetailsCard label="Nearest Street">
+            <h4>{data.nearstStreet}</h4>
+          </DetailsCard>
+        </div>
+        <div className="report-buttons">
+          <button className="report-button report-button__print"></button>
+          <label className="report-button report-button__prioritize" title="Prioritize">
+            <input
+              type="checkbox"
+              // onChange={this.handleCheckboxChange}
+              className="checkbox"
+            />
+            <span className="checkmark"></span>
+          </label>
+          <label className="report-button report-button__under-review" title="Prioritize">
+            <input
+              type="checkbox"
+              // onChange={this.handleCheckboxChange}
+              className="checkbox"
+            />
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div className="center-items">
+          <button className="report-button report-button__delete">Delete</button>
+          <button className="report-button report-button__return">Return</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ReportPage
+export default ReportPage;
